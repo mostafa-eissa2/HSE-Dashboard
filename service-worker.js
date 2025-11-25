cc0acae14edd5146ad74ec6c11aa979ff82fcd2c
mostafa-eissa2 <mostafaeissa272@gmail.com>
2025-11-25 10:43:20 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'hse-dashboard-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/system.html',
+  '/TURNKEY.png',
+  '/TURNKEY3.png',
+  '/manifest.json',
+  'https://d3js.org/d3.v7.min.js'
+];
+
+// 1. Install Service Worker & Cache Files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 2. Fetch Files (Serve from Cache if offline)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // If found in cache, return it; otherwise, fetch from network
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// 3. Activate & Clean up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
